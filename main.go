3ef7ae37e1bf1e4c3e8ea4606367bdd99ade4e97
diabeticMike <tmymichael@gmail.com>
2021-02-22 21:56:49 +0200

    update main

--- main.go
+++ main.go
@@ -5,26 +5,47 @@ import (
 	baseLog "log"
 
 	"github.com/PostService/infrastructure/config"
+	"github.com/PostService/infrastructure/datastore"
 	"github.com/PostService/infrastructure/logger"
+	"github.com/PostService/internal/post/cache"
+	"github.com/go-redis/redis"
 )
 
 func main() {
 	configFilePath := "config.json"
 	var (
-		Config config.Configuration
-		log    logger.Logger
-		// redisClient *redis.Client
-		err error
+		conf        config.Configuration
+		log         logger.Logger
+		redisClient *redis.Client
+		err         error
 	)
 
 	// Create service configuration
-	if Config, err = config.New(configFilePath); err != nil {
+	if conf, err = config.New(configFilePath); err != nil {
 		baseLog.Fatal(err.Error())
 	}
-	fmt.Println(Config)
+	fmt.Println(conf)
 
 	// Create service logger
-	if log, err = logger.New(Config.Log); err != nil {
+	if log, err = logger.New(conf.Log); err != nil {
 		baseLog.Fatal(err.Error())
 	}
+	fmt.Println(log)
+
+	if redisClient, err = datastore.NewRedis(redis.Options{
+		Addr:     conf.Redis.Address,
+		Password: conf.Redis.Password,
+		DB:       conf.Redis.DB,
+	}); err != nil {
+		log.Fatal(err.Error())
+	}
+	pIntf := cache.NewPostCache(redisClient)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	resp, err := pIntf.GetPostsByKey("shit")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println(resp)
 }
