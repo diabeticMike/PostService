99d3c37136fad12da1a2af273da24084e0f441de
diabeticMike <tmymichael@gmail.com>
2021-02-23 11:28:41 +0200

    add first endpoints

--- web/controller/post.go
+++ web/controller/post.go
@@ -9,6 +9,11 @@ import (
 	"github.com/PostService/model"
 )
 
+// NewPostController return PostController instance by passing log and post's business logic interface
+func NewPostController(log logger.Logger, postSvc post.Service) *PostController {
+	return &PostController{log: log, postSvc: postSvc}
+}
+
 // PostController responsible for holding logger and interface for post business logic
 type PostController struct {
 	log     logger.Logger
@@ -23,7 +28,7 @@ func (pc *PostController) InsertPost(w http.ResponseWriter, r *http.Request) {
 		pc.log.Error(err.Error())
 		return
 	}
-	if err := pc.postSvc.InsertPost(post, r.Body); err != nil {
+	if err := pc.postSvc.InsertPost(post); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		pc.log.Error(err.Error())
 		return
@@ -32,4 +37,51 @@ func (pc *PostController) InsertPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte(`Information stored successfully`))
 	w.WriteHeader(http.StatusOK)
+}
+
+// GetPosts posts objects
+func (pc *PostController) GetPosts(w http.ResponseWriter, r *http.Request) {
+	qParams := r.URL.Query()
+	author := qParams.Get("author")
+	name := qParams.Get("post_name")
+	var (
+		err   error
+		posts []model.Post
+	)
+	if author != "" && name != "" {
+		posts, err = pc.postSvc.GetPostsByNameAndAuthor(name, author)
+		if err != nil {
+			pc.log.Error(err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else if author != "" || name != "" {
+		var key string
+		if author != "" {
+			key = author
+		}
+		if name != "" {
+			key = name
+		}
+		if posts, err = pc.postSvc.GetPostsByKey(key); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			pc.log.Error(err.Error())
+			return
+		}
+	}
+
+	responce, err := json.Marshal(posts)
+	if err != nil {
+		pc.log.Error(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(responce); err != nil {
+		pc.log.Error(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
--- web/router/router.go
+++ web/router/router.go
@@ -1,20 +1,28 @@
 package router
 
 import (
-	"database/sql"
+	"net/http"
 
 	"github.com/PostService/infrastructure/logger"
+	"github.com/PostService/internal/post"
+	postCache "github.com/PostService/internal/post/cache"
+	"github.com/PostService/web/controller"
+	"github.com/go-redis/redis"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 // New base router
-func New(log logger.Logger, db *sql.DB) (router *mux.Router,
+func New(log logger.Logger, rc *redis.Client) (router *mux.Router,
 	headers handlers.CORSOption,
 	methods handlers.CORSOption,
 	origins handlers.CORSOption,
 	err error) {
 	router = mux.NewRouter().StrictSlash(true)
+
+	postSvc := post.NewPostService(postCache.NewPostCache(rc))
+	postCntr := controller.NewPostController(log, postSvc)
+	router.HandleFunc("/post", postCntr.InsertPost).Methods(http.MethodPost)
 
 	headers = handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
 	methods = handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
